0ccd0244eabaf35c7ff9786eaf95e63fe40287c2
Carla domínguez <carladominguez504@gmail.com>
2024-11-12 17:18:12 +0100

    acabamos de hacer nuestra clase cliente

--- src/main/java/org/example/Cliente.java
+++ src/main/java/org/example/Cliente.java
@@ -1,4 +1,26 @@
 package org.example;
 
-public class Cliente {
+class Cliente implements Runnable {
+    private String nombre;
+    private String[] productos;
+    private Caja caja;
+
+    public Cliente(String nombre, String[] productos, Caja caja) {
+        this.nombre = nombre;
+        this.productos = productos;
+        this.caja = caja;
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (String producto : productos) {
+                caja.procesarProducto(nombre, producto);  // Pasar producto a la caja
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+
