b46e6c8a6e896432f0feb173ac010cb29b33c7f5
Carla domínguez <carladominguez504@gmail.com>
2024-11-12 17:19:09 +0100

    Hemos echo nuestra clase caja que ese sería nuestro main

--- src/main/java/org/example/Caja.java
+++ src/main/java/org/example/Caja.java
@@ -1,5 +1,19 @@
 package org.example;
 
 class Caja {
+    private int id;
 
+    public Caja(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    // Método para procesar productos
+    public synchronized void procesarProducto(String cliente, String producto) throws InterruptedException {
+        Thread.sleep(1000); // Simula el tiempo de procesamiento de un producto
+        System.out.println("Caja " + id + " procesando producto: " + producto + " del cliente: " + cliente);
+    }
 }
